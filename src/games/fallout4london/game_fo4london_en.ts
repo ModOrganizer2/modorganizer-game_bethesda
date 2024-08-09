505f4f772e297ad9436413b0808b32533db837b7
Mikaël Capelle <capelle.mikael@gmail.com>
2024-08-09 17:47:51 +0200

    Minor fixes.

--- src/games/fallout4london/game_fo4london_en.ts
+++ src/games/fallout4london/game_fo4london_en.ts
@@ -9,17 +9,12 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gamefo4london.cpp" line="114"/>
-        <source>Adds support for the game Fallout 4 London.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="gamefo4london.cpp" line="350"/>
+        <location filename="gamefo4london.cpp" line="334"/>
         <source>sTestFile entries are present</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gamefo4london.cpp" line="358"/>
+        <location filename="gamefo4london.cpp" line="342"/>
         <source>&lt;p&gt;You have sTestFile settings in your Fallout4Custom.ini. These must be removed or the game will not read the plugins.txt file. Management is disabled.&lt;/p&gt;</source>
         <translation type="unfinished"></translation>
     </message>
