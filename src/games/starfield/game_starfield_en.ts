0e094706295a44d6253295e2c03d66b415c3c252
Jeremy Rimpo <jrim@rimpo.org>
2026-05-21 23:13:48 -0500

    Translation file updates

--- src/games/starfield/game_starfield_en.ts
+++ src/games/starfield/game_starfield_en.ts
@@ -24,30 +24,55 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gamestarfield.cpp" line="401"/>
+        <location filename="gamestarfield.cpp" line="444"/>
         <source>You have active ESP plugins in Starfield</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gamestarfield.cpp" line="403"/>
+        <location filename="gamestarfield.cpp" line="446"/>
         <source>sTestFile entries are present</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gamestarfield.cpp" line="413"/>
+        <location filename="gamestarfield.cpp" line="448"/>
+        <source>Invalid blueprint plugins found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gamestarfield.cpp" line="450"/>
+        <source>Unpaired blueprint plugins found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gamestarfield.cpp" line="460"/>
         <source>&lt;p&gt;ESP plugins are not ideal for Starfield. In addition to being unable to sort them alongside ESM or master-flagged plugins, certain record references are always kept loaded by the game. This consumes unnecessary resources and limits the game&apos;s ability to load what it needs.&lt;/p&gt;&lt;p&gt;Ideally, plugins should be saved as ESM files upon release. It can also be released as an ESL plugin, however there are additional concerns with the way light plugins are currently handled and should only be used when absolutely certain about what you&apos;re doing.&lt;/p&gt;&lt;p&gt;Notably, xEdit does not currently support saving ESP files.&lt;/p&gt;&lt;h4&gt;Current ESPs:&lt;/h4&gt;&lt;p&gt;%1&lt;/p&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gamestarfield.cpp" line="427"/>
+        <location filename="gamestarfield.cpp" line="474"/>
         <source>&lt;p&gt;You have plugin managment enabled but you still have sTestFile settings in your StarfieldCustom.ini. These must be removed or the game will not read the plugins.txt file. Management is still disabled.&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gamestarfield.cpp" line="479"/>
+        <source>&lt;p&gt;You have a blueprint file that is invalid. Blueprints require the blueprint flag and prefix and must have the ESM extension to be valid.&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gamestarfield.cpp" line="483"/>
+        <source>&lt;p&gt;You have a valid blueprint file that has no paired main plugin. The only way to load blueprint files is by enabling a main plugin with the same base name. This is the part after the prefix and before the extension.&lt;/p&gt;&lt;p&gt;eg. &lt;strong&gt;BlueprintShips-example.esm&lt;/strong&gt; should have a paired main plugin &lt;strong&gt;example.esm&lt;/strong&gt;&lt;/p&gt;</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>QObject</name>
     <message>
-        <location filename="starfieldunmanagedmods.cpp" line="70"/>
+        <location filename="starfieldgameplugins.cpp" line="85"/>
+        <source>Some of your plugins have invalid names! These plugins can not be loaded by the game. Please see mo_interface.log for a list of affected plugins and rename them.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="starfieldunmanagedmods.cpp" line="93"/>
         <source>ContentCatalog.txt appears to be corrupt: %1</source>
         <translation type="unfinished"></translation>
     </message>
